974a4ca865bc67074bf236f7cb8b3f07006e0861
Gahh-gon <77693978+Gahh-gon@users.noreply.github.com>
2022-09-13 16:58:00 -0300

    login

---
+++ projeto_TCC/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'projeto_TCC';
+
+   caminho = "../assets/IMG/pexels-dapurmelodi-1109197.jpg"
+}
+
